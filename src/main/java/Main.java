6e8bef6a07e4262df63d876ac5e2f480c181289f
essaghir.iman@gmail.com <gh.19.IB@na>
2020-02-12 22:44:38 -0500

    Insert fields to author table, warning related to SQL and table not found, will be fixed soon

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,9 +3,7 @@ import com.google.gson.stream.JsonReader;
 import com.opencsv.exceptions.CsvValidationException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
+import java.sql.*;
 
 /* Code provided by Professor Josiah Greenwell */
 
@@ -17,13 +15,12 @@ public class Main {
     static final String PASS = "";
     static Connection conn;
 
-    public static void main(String[] args) throws IOException, CsvValidationException {
+    public static void main(String[] args) throws IOException, CsvValidationException, ClassNotFoundException, SQLException {
         // Call the parser
         CsvParser csvP = new CsvParser("src/Data/bookstore_report2.csv");
         csvP.printCsv();
 
-        // Load a json file:
-        //  Create instance of GSON
+        // Load a json file;  Create instance of GSON
         Gson gson = new Gson();
 
         // Create a JsonReader object using FileReader
@@ -39,6 +36,23 @@ public class Main {
 
         System.out.println();
         testDatabaseConnection();
+        System.out.println();
+
+        String sql = "INSERT INTO author (author_name, author_email, author_url) VALUES  (? , ? , ?)";
+        Class.forName(JDBC_DRIVER);
+        conn = DriverManager.getConnection(DB_URL);
+        Statement stmt = conn.createStatement();
+        PreparedStatement ps = conn.prepareStatement(sql);
+
+        for (var element : authors) {
+            ps.setString(1, element.getName());
+            ps.setString(2, element.getEmail());
+            ps.setString(3, element.getUrl());
+        }
+
+        ps.close();
+        stmt.close();
+        conn.close();
 
     }
 
@@ -48,6 +62,7 @@ public class Main {
             Class.forName(JDBC_DRIVER);
             conn = DriverManager.getConnection(DB_URL, USER, PASS);
             System.out.println("Database Connected!");
+
         } catch (Exception ex) {
             ex.printStackTrace();
             System.out.println("Database Failed to Connect!");
